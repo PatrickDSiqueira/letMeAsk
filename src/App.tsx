1e5025b683cbf9f5f0618adda33d6536b0a302d8
PatrickDSiqueira <psique.estuda@gmail.com>
2022-04-18 14:53:45 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-      <NewRoom/>
+    // <Home/>    // teste
+    // <NewRoom/>  // teste
+    <BrowserRouter>
+    <Routes>
+
+      <Route index element={<Home />} />
+
+      <Route  path="/" element={<Home/>} />
+      <Route path="/rooms/new" element={<NewRoom/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,20 @@
+import { Navigate, useNavigate } from 'react-router-dom'
+
 import illustration from '../assets/images/illustration.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 import logo from '../assets/images/logo.svg'
+
 import {Button} from '../components/Button'
 
 import '../styles/auth.scss'
 
 export function Home(){
+    const navigate = useNavigate();
+
+    function navigateToNewRoom(){
+        navigate('/rooms/new')
+    }
+
     return(
         <div id='page-auth'>
              <aside>
@@ -16,8 +25,8 @@ export function Home(){
              <main>
                  <div className='main-content'>
                      <img src={logo} alt="letmeask" />
-                     <button className='create-room'>
-                       <img src={googleIconImg} alt="Logo do Google" />
+                     <button className='create-room' >
+                       <img src={googleIconImg} alt="Logo do Google" onClick={ navigateToNewRoom }/>
                        Crie sua sala com o Google
                      </button>
                      <div className='separator'>ou entre em uma sala</div>
