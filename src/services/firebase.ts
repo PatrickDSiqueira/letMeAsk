d9f202b851438ef90282c3c55298e146a3a0b853
PatrickDSiqueira <psique.estuda@gmail.com>
2022-04-17 21:16:45 -0300

    Primeiro Commit

---
+++ src/services/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import 'firebase/auth';
+import 'firebase/database';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MASSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_IP
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// const auth = firebase.auth();
+// const database = firebase.database();
