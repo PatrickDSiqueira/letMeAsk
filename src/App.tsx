ac99bc9ec91d9b424392de0ca5b18021dac797e0
PatrickDSiqueira <psique.estuda@gmail.com>
2022-04-21 08:24:25 -0300

    criacao de um arquivo somente para a auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,29 @@
 
-import { createContext, useState } from 'react';
+import { createContext  } from 'react';
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { auth, firebase } from "./services/firebase";
 
-type User ={
-  id: string;
-  name: string;
-  avatar: string;
-  
-}
-type AuthContextType = {
-  user: User | undefined;
-  signInWithGoogle: () => Promise<void>;
-}
+import {AuthContextProvider} from "./contexts/AuthContext"
 
-export const AuthContext = createContext({}as AuthContextType);
+
 
 function App() {
-  const [user, setUser] = useState<User>();
   
-  async function signInWithGoogle(){
-    const provider = new firebase.auth.GoogleAuthProvider();
-    const result = await auth.signInWithPopup(provider);
-
-
-  
-  if (result.user){
-      const { displayName, photoURL, uid } = result.user;
-    if ( !displayName || !photoURL ){
-      throw new Error ('Missing information from Google Acount.')
-    }
-    setUser({
-       id : uid,
-      name: displayName,
-       avatar: photoURL
-    })
-     }
-  };
 
 
   return (
     <BrowserRouter>
-        <AuthContext.Provider value={{user, signInWithGoogle}}>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/rooms/new" element={<NewRoom/>}/>
-      </Routes>
-        </AuthContext.Provider>
+        <AuthContextProvider>
+          <Route path="/" element={<Home/>} />
+          <Route path="/rooms/new" element={<NewRoom/>}/>
+        </AuthContextProvider>
+        </Routes>
     </BrowserRouter>
+    
   );
   }
 export default App;
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,6 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../App';
 import illustration from '../assets/images/illustration.svg';
 import logo from '../assets/images/logo.svg';
 import {Button} from '../components/Button';
@@ -6,6 +8,8 @@ import {Button} from '../components/Button';
 import '../styles/auth.scss';
 
 export function NewRoom(){
+    const {user} = useContext(AuthContext);
+
     return(
         <div id='page-auth'>
              <aside>
@@ -16,13 +20,13 @@ export function NewRoom(){
              <main>
                  <div className='main-content'>
                      <img src={logo} alt="letmeask" />
+                     <h1>{user?.name}</h1>
                      <h2>Crie uma nova sala</h2>
                      <form>
                          <input type="text" placeholder='Nome da sala' />
                          <Button type='submit'>Criar sala</Button>
                      </form>
-                     <p>Quer entrar em uma sala já existente? <Link to="/">Clique aqui</Link></p>
-                      
+                     <p>Quer entrar em uma sala já existente? <Link to="/">Clique aqui</Link></p> 
                  </div>
              </main>
         </div>
